ff8af4abf7ce1b5f142d380db82c2dcc3515f297
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-01-26 13:04:43 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/training/springwebsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/training/springwebsecurity/config/WebSecurityConfig.java
@@ -1,31 +1,36 @@
 package com.training.springwebsecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password(passwordEncoder().encode("user")).roles("USER")
-				.and()
-				.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("admin").password("admin").authorities("ADMIN").build();
+		userDetailsService.createUser(user);
+		return userDetailsService;
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
 	}
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
