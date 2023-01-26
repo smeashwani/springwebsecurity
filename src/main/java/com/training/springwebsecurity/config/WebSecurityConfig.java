54fdd229d68ccc121841e2b889f1e3dd36ba84b1
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-01-26 08:21:38 +0530

    update

---
+++ src/main/java/com/training/springwebsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.training.springwebsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+				.and()
+				.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/").permitAll()  // it allow for anyuser
+			.antMatchers("/admin").authenticated() // check authentication 
+			.antMatchers("/admin").hasAnyRole("ADMIN")  // check authorization 
+			.and().formLogin().permitAll();
+	}
+}
