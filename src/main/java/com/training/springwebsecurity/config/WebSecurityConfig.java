933bab6385965f9b92ca8b037720391df0b7f520
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-09-04 14:13:21 +0530

    update

--- src/main/java/com/training/springwebsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/training/springwebsecurity/config/WebSecurityConfig.java
@@ -1,41 +1,59 @@
 package com.training.springwebsecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password(passwordEncoder().encode("user")).roles("USER")
-				.and()
-				.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-	}
-
+public class WebSecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/").permitAll()  // it allow for anyuser
-			.antMatchers("/admin").authenticated() // check authentication 
-			.antMatchers("/admin").hasAnyRole("ADMIN")  // check authorization 
-			.and()
-			.formLogin().permitAll()
-	        .and()
-	        .logout()
-	        .logoutUrl("/logout")  
-	        .logoutSuccessUrl("/");	
-		}
+	
+	@Bean
+ 	public UserDetailsService userDetailsService() {
+		
+ 		UserDetails user1 = User.withUsername("user")
+ 			.password(passwordEncoder().encode("user"))
+ 			.roles("USER")
+ 			.build();
+ 		
+ 		UserDetails user2 = User.withUsername("admin")
+ 	 			.password(passwordEncoder().encode("admin"))
+ 	 			.roles("ADMIN")
+ 	 			.build();
+ 		return new InMemoryUserDetailsManager(user1, user2);
+ 	}
+	
+	@Bean
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+	    return new MvcRequestMatcher.Builder(introspector);
+	}
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+            		.requestMatchers(mvc.pattern("/")).permitAll()
+            		.requestMatchers(mvc.pattern("/admin")).hasRole("ADMIN")
+            )
+        .formLogin((temp) -> {System.out.println("http form operation performed");})
+        .logout((temp) -> {
+        	temp.logoutRequestMatcher(mvc.pattern("/logout"));
+        	temp.logoutSuccessUrl("/");
+         });
+        return http.build();
+    }
 }
